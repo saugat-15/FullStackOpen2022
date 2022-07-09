ce36652c053f7c4b78896c788a5f4f8a8fb523fa
Prasanga Shrestha <saugatgiri15@gmail.com>
2022-07-09 15:37:24 +1000

    part1 completed/anecdotes(struggled with counting most voted)

--- src/App.js
+++ src/App.js
@@ -1,69 +1,54 @@
 import { useState } from "react";
 import "./App.css";
 
-const Header = (props) => {
+
+const MostVotedAnecdotes = ({ anecdotes, votes}) => {
+  const maxVotes = Math.max(...votes)
+  const mostVotedAnecdote = votes.indexOf(maxVotes)
+  
   return (
     <div>
-      <h3>{ props.title}</h3>
-    </div>
-  )
-};
-
-const Statistics = ({good, bad, neutral}) => {
-  const countAll = () => {
-    return good + neutral + bad;
-  }
-
-  const countAverage = () => {
-    return ((good + neutral) - bad) / 3
-  }
-
-  const calculatePositive = () => {
-    return (good + neutral) / (good + bad + neutral) * 100
-  }
-  return (
-    <div>
-        <p>Good: { good}</p>
-        <p>Neutral: { neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {countAll()}</p>
-        <p>Average: {countAverage()}</p>
-        <p>Positive: {calculatePositive()}</p>
-        
+    <p>Most Voted Anecdote:</p>
+      <p>{anecdotes[mostVotedAnecdote]}</p>
       </div>
+
   )
 }
-
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0);
+  const [votes, setVote] = useState(Array(anecdotes.length).fill(0));
 
-  const countGood = () => {
-    setGood(good + 1);
-  };
-
-  const countNeutral = () => {
-    setNeutral(neutral + 1);
-  };
-
-  const countBad = () => {
-    setBad(bad + 1);
+  const randomAnecdote = () => {
+    const randomNum = Math.round(Math.random() * anecdotes.length)
+    setSelected(randomNum)
   }
-  const condition = good !== 0 || bad !== 0 || neutral !== 0;
+
+  const voteCount = () => {
+    const newVote = [...votes];
+    newVote[selected] += 1;
+    setVote(newVote)
+  }
+
+
   return (
-    <div className="App">
-      <Header title="Give Feedback" />
-      <div>
-        <button onClick={countGood}>good</button>
-        <button onClick={countNeutral}>neutral</button>
-        <button onClick={countBad}>bad</button>
-      </div>
-      {condition && <Statistics good={ good} bad={bad} neutral={neutral} />}
-      {/* <Content /> */}
+    <div>
+      {anecdotes[selected]}
+      <br />
+      <button onClick={randomAnecdote}>Next</button>
+      <button onClick={voteCount}>Vote</button>
+      <h4>Anecdote with the most votes:</h4>
+      <MostVotedAnecdotes anecdotes={anecdotes} votes={ votes} />
     </div>
-  );
+  )
 };
 export default App;
